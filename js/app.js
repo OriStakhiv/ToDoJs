8c97202e6ae82acff17cb653d7ed4dc0cd65ccba
OriStakhiv <orysiastakh@gmail.com>
2018-04-03 13:57:33 +0300

    added js code, need to complite  bindEvents function

---
+++ js/app.js
@@ -0,0 +1,54 @@
+function createTodoItem(title) {
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.className = 'checkbox';
+
+  const label = document.createElement('label');
+  label.innerText = title;
+  label.className = 'title';
+
+  const editInput = document.createElement('input');
+  editInput.type = 'text';
+  editInput.className = 'textfield';
+
+  const  editButton = document.createElement('button');
+  editButton.innerText = 'Edit';
+  editButton.className = 'edit';
+  const deleteButton = document.createElement('button');
+  deleteButton.innerText = 'Delete';
+  deleteButton.className = 'delete';
+
+  const li = document.createElement('li');
+  listItem.className = 'todo-item';
+
+  listItem.appendChild(checkbox);
+  listItem.appendChild(lable);
+  listItem.appendChild(editInput);
+  listItem.appendChild(editButton);
+  listItem.appendChild(deleteButton);
+
+  return listItem;
+
+}
+ function bindEvents(todoItem){
+   const checkbox = todoItem.querySelector('.checkbox');
+   const editButton = todoItem.querySelector('button.edit');
+   const deleteButton = todoItem.querySelector('button.delete');
+
+ }
+function addTodoItem(event) {
+  event.preventDefault();
+
+  if(addInput.value === '')
+  return alert('You need to write task!');
+
+  const todoItem = createTodoItem(addInput.value);
+  todoList.appendChild(todoItem)
+  addInput.value = '';
+}
+const todoForm = document.createElement('todo-form');
+const addInput = document.createElement('add-input');
+const todoList = document.getElementById('todo-list');
+const todoItems = document.querySelectorAll('.todo-item');
+
+todoForm.addEventListener('submit',addTodoItem);
